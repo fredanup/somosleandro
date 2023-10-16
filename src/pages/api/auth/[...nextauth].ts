7bc8feb680c1cc264538f9661d3c7e6a8064f6a3
Angel Ugarte <fredanup@hotmail.com>
2023-10-16 18:26:03 -0500

    actualizando dependencias

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,61 +1,51 @@
-import NextAuth from 'next-auth';
-import { AppProviders } from 'next-auth/providers';
-import CredentialsProvider from 'next-auth/providers/credentials';
-import GithubProvider from 'next-auth/providers/github';
+import NextAuth, { type NextAuthOptions } from "next-auth";
+
+// Prisma adapter for NextAuth, optional and can be removed
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import { prisma } from "server/prisma";
+import { env } from "../../../server/env";
+
 import GoogleProvider from "next-auth/providers/google";
 import FacebookProvider from "next-auth/providers/facebook";
 
-let useMockProvider = process.env.NODE_ENV === 'test';
-const { GITHUB_CLIENT_ID, GITHUB_SECRET, GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET, NODE_ENV, APP_ENV } = process.env;
-if (
-  (NODE_ENV !== 'production' || APP_ENV === 'test') &&
-  (!GOOGLE_CLIENT_ID || !GOOGLE_CLIENT_SECRET)
-) {
-  console.log('⚠️ Using mocked Google auth correct credentials were not added');
-  useMockProvider = true;
-}
-const providers: AppProviders = [];
-if (useMockProvider) {
-  providers.push(
-    CredentialsProvider({
-      id: 'github',
-      name: 'Mocked GitHub',
-      async authorize(credentials) {
-        if (credentials) {
-          const name = credentials.name;
-          return {
-            id: name,
-            name: name,
-            email: name,
-          };
-        }
-        return null;
-      },
-      credentials: {
-        name: { type: 'test' },
-      },
-    }),
-  );
-} else {
-  if (!GOOGLE_CLIENT_ID || !GOOGLE_CLIENT_SECRET) {
-    throw new Error('GOOGLE_CLIENT_ID and GOOGLE_CLIENT_SECRET must be set');
-  }
-  providers.push(
-    GoogleProvider({
-      clientId: GOOGLE_CLIENT_ID,
-      clientSecret: GOOGLE_CLIENT_SECRET,
-      profile(profile) {
-        return {
-          id: profile.id,
-          name: profile.login,
-          email: profile.email,
-          image: profile.avatar_url,
-        } as any;
-      },
-    }),
-  );
-}
-export default NextAuth({
+export const authOptions: NextAuthOptions = {
+  // Include user.id on session
+  // callbacks: {
+  //   session({ session, user }) {
+  //     if (session.user) {
+  //       session.user.id = user.id;
+  //     }
+  //     return session;
+  //   },
+  // },
   // Configure one or more authentication providers
-  providers,
-});
+
+  callbacks: {
+    session: ({ session, user }) => ({
+      ...session,
+      user: {
+        ...session.user,
+        id: user.id,
+      },
+    }),
+   
+  },
+  adapter: PrismaAdapter(prisma),
+  providers: [
+    // ...add more providers here
+    GoogleProvider({
+      clientId: env.GOOGLE_CLIENT_ID,
+      clientSecret: env.GOOGLE_CLIENT_SECRET,     
+
+    }),
+    FacebookProvider({
+      clientId: env.FACEBOOK_CLIENT_ID,
+      clientSecret: env.FACEBOOK_CLIENT_SECRET,
+    }),
+   
+  ],
+  secret: env.NEXTAUTH_SECRET,
+
+};
+
+export default NextAuth(authOptions);
