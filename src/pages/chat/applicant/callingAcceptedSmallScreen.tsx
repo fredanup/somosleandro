5e76cf866150a9c53ec5715889098869851f9973
Angel Ugarte <fredanup@hotmail.com>
2023-11-07 08:22:40 -0500

    Estilizado de menú

--- src/pages/chat/applicant/callingAcceptedSmallScreen.tsx
+++ src/pages/chat/applicant/callingAcceptedSmallScreen.tsx
@@ -43,9 +43,15 @@ export default function CallingAcceptedSmallScreen({
   return (
     <>
       {!userApplicationsAccepted || userApplicationsAccepted.length === 0 ? (
-        <li className="m-6 text-slate-500">
-          Usted no ha creado ninguna convocatoria
-        </li>
+        <div className="flex flex-row gap-4 m-6">
+          <svg className="h-6 fill-gray-400" viewBox="0 0 512 512">
+            <path d="M256 512A256 256 0 1 0 256 0a256 256 0 1 0 0 512zM159.3 388.7c-2.6 8.4-11.6 13.2-20 10.5s-13.2-11.6-10.5-20C145.2 326.1 196.3 288 256 288s110.8 38.1 127.3 91.3c2.6 8.4-2.1 17.4-10.5 20s-17.4-2.1-20-10.5C340.5 349.4 302.1 320 256 320s-84.5 29.4-96.7 68.7zM144.4 208a32 32 0 1 1 64 0 32 32 0 1 1 -64 0zm192-32a32 32 0 1 1 0 64 32 32 0 1 1 0-64z" />
+          </svg>
+
+          <p className="text-slate-500">
+            Que mala suerte, aún nadie te ha elegido, pero sigue postulando
+          </p>
+        </div>
       ) : (
         userApplicationsAccepted?.map((entry, index) => (
           <div
