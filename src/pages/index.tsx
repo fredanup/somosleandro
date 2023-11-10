69b89d204908080c0904dc90ea003f95db4aea74
Angel Ugarte <fredanup@hotmail.com>
2023-11-09 19:47:31 -0500

    Unificado de márgenes y padding en login

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -31,28 +31,28 @@ export default function Home() {
 
   return (
     <>
-      <div className="md:bg-slate-100 md:flex md:flex-row ">
-        {/**Envuelve a dos contenedores y content-between es para que la opción de registro se encuentre en la parte inferior sin salirse de su contenedor bg-[url('/images/carito.jpg')] bg-cover bg-center */}
-        <div className="flex flex-col h-screen rounded-lg bg-white md:order-first px-6 md:w-2/5">
+      <div className="h-screen w-screen bg-white rounded-lg md:flex md:flex-row">
+        {/**Contenedor de la izquierda*/}
+        <div className="flex flex-col h-full w-full px-4 md:order-first md:w-2/5">
           {/**Header */}
-          <div className="mt-8 mb-2">
-            <h1 className="font-poppins text-3xl text-center text-gray-600 font-bold mt-16">
+          <div className="flex-none items-center flex flex-col pt-16 gap-4">
+            <h1 className="font-poppins text-3xl text-gray-600 font-bold">
               ¡Bienvenido!
             </h1>
             <Image
-              className="mt-6 h-20 w-full justify-center items-center"
+              className="h-24 w-24 items-center"
               src="/icons/Logo.svg"
               width={100}
               height={100}
               alt="Logo"
             />
-            <p className="text-center text-base font-light text-gray-500 mt-6">
+            <p className="text-base font-light text-gray-500">
               Por favor, Ingrese sus datos
             </p>
           </div>
           {/**Body */}
           <form
-            className="mb-8"
+            className="grow flex flex-col gap-4 w-full h-full px-6 mt-4"
             onSubmit={(event) => {
               event.preventDefault();
               signIn('credentials', {
@@ -64,8 +64,8 @@ export default function Home() {
           >
             {/**Input text y label*/}
 
-            <div className="relative w-full px-6">
-              <label className="mb-2 block text-lg font-medium text-black">
+            <div className="relative w-full flex flex-col gap-2">
+              <label className="block text-lg font-semibold text-slate-900">
                 E-mail
               </label>
               <input
@@ -73,10 +73,10 @@ export default function Home() {
                 type="email"
                 value={email}
                 onChange={(event) => setEmail(event.target.value)}
-                className=" w-full rounded-lg border pl-12 px-3 py-2 leading-tight text-gray-700 shadow focus:outline-none focus:border-violet-300 focus:ring-violet-300"
+                className="w-full rounded-lg border pl-12 px-3 py-2 leading-tight text-gray-700 shadow focus:outline-none focus:border-violet-300 focus:ring-violet-300"
               />
               <svg
-                className="absolute h-6 fill-gray-300 bottom-2 left-9"
+                className="absolute h-6 fill-gray-300 bottom-2 left-3"
                 viewBox="0 0 512 512"
               >
                 <path d="M48 64C21.5 64 0 85.5 0 112c0 15.1 7.1 29.3 19.2 38.4L236.8 313.6c11.4 8.5 27 8.5 38.4 0L492.8 150.4c12.1-9.1 19.2-23.3 19.2-38.4c0-26.5-21.5-48-48-48H48zM0 176V384c0 35.3 28.7 64 64 64H448c35.3 0 64-28.7 64-64V176L294.4 339.2c-22.8 17.1-54 17.1-76.8 0L0 176z" />
@@ -84,8 +84,8 @@ export default function Home() {
             </div>
 
             {/**Input text y label*/}
-            <div className="relative w-full px-6 mt-4">
-              <label className="mb-2 block text-lg font-medium text-black">
+            <div className="relative w-full flex flex-col gap-2">
+              <label className="block text-lg font-semibold text-slate-900">
                 Contraseña
               </label>
               <input
@@ -96,28 +96,28 @@ export default function Home() {
                 className=" w-full rounded-lg border pl-12 px-3 py-2 leading-tight text-gray-700 shadow focus:outline-none focus:border-violet-300 focus:ring-sky-300"
               />
               <svg
-                className="absolute h-6 fill-gray-300 bottom-2 left-9"
+                className="absolute h-6 fill-gray-300 bottom-2 left-3"
                 viewBox="0 0 512 512"
               >
                 <path d="M336 352c97.2 0 176-78.8 176-176S433.2 0 336 0S160 78.8 160 176c0 18.7 2.9 36.8 8.3 53.7L7 391c-4.5 4.5-7 10.6-7 17v80c0 13.3 10.7 24 24 24h80c13.3 0 24-10.7 24-24V448h40c13.3 0 24-10.7 24-24V384h40c6.4 0 12.5-2.5 17-7l33.3-33.3c16.9 5.4 35 8.3 53.7 8.3zM376 96a40 40 0 1 1 0 80 40 40 0 1 1 0-80z" />
               </svg>
             </div>
 
-            {/**Botones*/}
-            <div className="mt-10 flex flex-col items-center justify-center px-6">
+            {/**Botones y cartilla*/}
+            <div className="mt-4 flex flex-col items-center gap-4">
               {/**Botón de inicio */}
               <button
                 type="submit"
-                className="w-full mb-6 h-10 rounded-lg bg-sky-500 px-3 py-1.5 text-base font-semibold text-white hover:border-transparent hover:bg-sky-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-sky-400 focus:ring-offset-2"
+                className="w-full h-10 rounded-lg bg-sky-500 text-base font-semibold text-white hover:border-transparent hover:bg-sky-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-sky-400 focus:ring-offset-2"
               >
                 Iniciar sesión
               </button>
               {/**Label de alternativa */}
-              <p className="mb-4 mt-2 text-base font-light text-gray-400">
+              <p className="text-base font-light text-gray-400">
                 o continuar con:
               </p>
               {/**Botones de redes sociales */}
-              <div className="items-center justify-content flex flex-row gap-4">
+              <div className="items-center flex flex-row gap-4">
                 <svg
                   className="h-8 cursor-pointer"
                   viewBox="0 0 488 512"
@@ -140,11 +140,11 @@ export default function Home() {
                   <path d="M504 256C504 119 393 8 256 8S8 119 8 256c0 123.78 90.69 226.38 209.25 245V327.69h-63V256h63v-54.64c0-62.15 37-96.48 93.67-96.48 27.14 0 55.52 4.84 55.52 4.84v61h-31.28c-30.8 0-40.41 19.12-40.41 38.73V256h68.78l-11 71.69h-57.78V501C413.31 482.38 504 379.78 504 256z" />
                 </svg>
               </div>
+              <p className="text-s text-center text-base font-light text-gray-400">
+                Eres nuevo? <span className="text-sky-500">Regístrate</span>
+              </p>
             </div>
           </form>
-          <p className="text-s text-center text-base font-light text-gray-400">
-            Eres nuevo? <span className="text-sky-500">Regístrate</span>
-          </p>
         </div>
         {/**Este contenedor no aparece en móviles */}
         <div className="hidden md:order-last md:block md:h-screen md:w-3/5 md-rounded-tr-lg md-rounded-br-lg md-rounded-none md:bg-[url('/images/chucky.jpg')] md:bg-cover md:bg-center"></div>
--- src/pages/main.tsx
+++ src/pages/main.tsx
@@ -105,12 +105,12 @@ export default function Main() {
 
   return (
     <>
-      <div className="m-0 box-border h-screen w-screen border-0 bg-slate-200 drop-shadow-lg rounded-lg md:p-2 md:flex md:flex-row md:h-screen md:w-screen md:gap-2">
+      <div className="m-0 box-border h-screen w-screen border-0 bg-slate-200 drop-shadow-lg rounded-lg md:flex md:flex-row md:h-screen md:w-screen md:gap-2">
         {/**Nota como se puede apreciar hay un desborde en la pantalla principal pero este es necesario para que el menú aparezca y desaparezca */}
 
         {/**Menú de navegación */}
         <nav
-          className={`fixed inset-x-0 bottom-0 z-10 h-12 border-t border-gray-200 flex flex-row bg-white drop-shadow-lg justify-evenly items-center md:static md:flex md:h-full md:w-12 md:flex-col md:rounded-full md:border-0 ${
+          className={`fixed inset-x-0 bottom-0 z-10 h-12 border-t border-gray-200 flex flex-row bg-white drop-shadow-lg justify-evenly items-center md:static md:border-r md:border-gray-200 md:flex md:h-full md:flex-col md:border-0 ${
             isMenuVisible ? 'block' : 'hidden'
           }`}
         >
@@ -120,18 +120,21 @@ export default function Main() {
         */}
           {/**Logo se oculta en dispositivos móviles */}
 
-          <svg
-            viewBox="0 0 512 512"
-            className={`h-8 w-8 cursor-pointer fill-gray-500 p-1.5  ${
-              opt === 1 ? 'fill-pink-500' : ''
-            }`}
-            onClick={() => {
-              setOpt(1);
-              setSelectedCard(null);
-            }}
-          >
-            <path d="M278.5 215.6L23 471c-9.4 9.4-9.4 24.6 0 33.9s24.6 9.4 33.9 0l74.8-74.8c7.4 4.6 15.3 8.2 23.8 10.5C200.3 452.8 270 454.5 338 409.4c12.2-8.1 5.8-25.4-8.8-25.4l-16.1 0c-5.1 0-9.2-4.1-9.2-9.2c0-4.1 2.7-7.6 6.5-8.8l97.7-29.3c3.4-1 6.4-3.1 8.4-6.1c4.4-6.4 8.6-12.9 12.6-19.6c6.2-10.3-1.5-23-13.5-23l-38.6 0c-5.1 0-9.2-4.1-9.2-9.2c0-4.1 2.7-7.6 6.5-8.8l80.9-24.3c4.6-1.4 8.4-4.8 10.2-9.3C494.5 163 507.8 86.1 511.9 36.8c.8-9.9-3-19.6-10-26.6s-16.7-10.8-26.6-10C391.5 7 228.5 40.5 137.4 131.6C57.3 211.7 56.7 302.3 71.3 356.4c2.1 7.9 12 9.6 17.8 3.8L253.6 195.8c6.2-6.2 16.4-6.2 22.6 0c5.4 5.4 6.1 13.6 2.2 19.8z" />
-          </svg>
+          <div className="flex flex-row items-center justify-center">
+            <svg
+              viewBox="0 0 512 512"
+              className={`h-8 w-8 cursor-pointer fill-gray-500 p-1.5  ${
+                opt === 1 ? 'fill-pink-500' : ''
+              }`}
+              onClick={() => {
+                setOpt(1);
+                setSelectedCard(null);
+              }}
+            >
+              <path d="M278.5 215.6L23 471c-9.4 9.4-9.4 24.6 0 33.9s24.6 9.4 33.9 0l74.8-74.8c7.4 4.6 15.3 8.2 23.8 10.5C200.3 452.8 270 454.5 338 409.4c12.2-8.1 5.8-25.4-8.8-25.4l-16.1 0c-5.1 0-9.2-4.1-9.2-9.2c0-4.1 2.7-7.6 6.5-8.8l97.7-29.3c3.4-1 6.4-3.1 8.4-6.1c4.4-6.4 8.6-12.9 12.6-19.6c6.2-10.3-1.5-23-13.5-23l-38.6 0c-5.1 0-9.2-4.1-9.2-9.2c0-4.1 2.7-7.6 6.5-8.8l80.9-24.3c4.6-1.4 8.4-4.8 10.2-9.3C494.5 163 507.8 86.1 511.9 36.8c.8-9.9-3-19.6-10-26.6s-16.7-10.8-26.6-10C391.5 7 228.5 40.5 137.4 131.6C57.3 211.7 56.7 302.3 71.3 356.4c2.1 7.9 12 9.6 17.8 3.8L253.6 195.8c6.2-6.2 16.4-6.2 22.6 0c5.4 5.4 6.1 13.6 2.2 19.8z" />
+            </svg>
+            <p className="hidden md:block">Publicaciones</p>
+          </div>
 
           <svg
             viewBox="0 0 640 512"
