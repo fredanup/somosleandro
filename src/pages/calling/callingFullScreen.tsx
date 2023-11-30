f545ebff265a3994a246057060a397da86201f82
Angel Ugarte <fredanup@hotmail.com>
2023-11-29 20:51:03 -0500

    Add stop propagation on svg button

--- src/pages/calling/callingFullScreen.tsx
+++ src/pages/calling/callingFullScreen.tsx
@@ -102,14 +102,22 @@ export default function CallingFullScreen({
   }, [selectedCard, applicantsQuery.data]);
 
   //Función que se utiliza para evitar una llamada directa a la mutación de rechazo
-  function rejectAppliantStatus(appliantId: string) {
+  function rejectAppliantStatus(
+    event: React.MouseEvent<SVGSVGElement>,
+    appliantId: string,
+  ) {
+    event.stopPropagation();
     rejectApplicant.mutate({
       id: appliantId,
     });
   }
 
   //Función que se utiliza para evitar una llamada directa a la mutación de aprobación
-  function acceptAppliantStatus(appliantId: string) {
+  function acceptAppliantStatus(
+    event: React.MouseEvent<SVGSVGElement>,
+    appliantId: string,
+  ) {
+    event.stopPropagation();
     acceptApplicant.mutate({
       id: appliantId,
     });
@@ -206,14 +214,14 @@ export default function CallingFullScreen({
                       className={`h-6 w-6 cursor-pointer fill-black ${
                         entry.applyStatus === 'accepted' ? 'hidden' : 'block'
                       }`}
-                      onClick={() => acceptAppliantStatus(entry.id)}
+                      onClick={(event) => acceptAppliantStatus(event, entry.id)}
                     >
                       <path d="M438.6 105.4c12.5 12.5 12.5 32.8 0 45.3l-256 256c-12.5 12.5-32.8 12.5-45.3 0l-128-128c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0L160 338.7 393.4 105.4c12.5-12.5 32.8-12.5 45.3 0z" />
                     </svg>
                     <svg
                       viewBox="0 0 512 512"
                       className="h-6 w-6 cursor-pointer fill-red-500"
-                      onClick={() => rejectAppliantStatus(entry.id)}
+                      onClick={(event) => rejectAppliantStatus(event, entry.id)}
                     >
                       <path d="M342.6 150.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L192 210.7 86.6 105.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L146.7 256 41.4 361.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L192 301.3 297.4 406.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L237.3 256 342.6 150.6z" />
                     </svg>
