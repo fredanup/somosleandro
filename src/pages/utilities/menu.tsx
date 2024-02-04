dea09c17f6b67247a9abe98c09b72b0ec135f9cd
Angel Ugarte <fredanup@hotmail.com>
2024-02-03 19:59:16 -0500

    Mejoras en menú

--- src/pages/utilities/menu.tsx
+++ src/pages/utilities/menu.tsx
@@ -19,36 +19,6 @@ const Menu = ({ optSelected }: { optSelected: number }) => {
     setOpt(optSelected);
   }, [optSelected]);
 
-  useEffect(() => {
-    const handleRouteChange = (url: string) => {
-      // Verificar si la nueva ruta es /applying/jobs
-      if (url === '/applying/jobs') {
-        // Cerrar la aplicación o realizar cualquier otra acción deseada
-        // En este ejemplo, simplemente cerramos la ventana/tab actual
-        window.close();
-      }
-    };
-
-    const handlePopState = () => {
-      // Manejar el evento de retroceso del navegador aquí
-      // Puedes verificar la ruta actual usando router.pathname
-      if (router.pathname === '/applying/jobs') {
-        // Cerrar la aplicación o realizar cualquier otra acción deseada
-        window.close();
-      }
-    };
-
-    // Suscribirse a los eventos
-    router.events.on('routeChangeComplete', handleRouteChange);
-    window.addEventListener('popstate', handlePopState);
-
-    // Limpiar suscripciones al desmontar el componente
-    return () => {
-      router.events.off('routeChangeComplete', handleRouteChange);
-      window.removeEventListener('popstate', handlePopState);
-    };
-  }, [router]);
-
   return (
     <>
       {/**
@@ -121,6 +91,7 @@ const Menu = ({ optSelected }: { optSelected: number }) => {
             Empleos
           </p>
         </div>
+
         <div className="flex flex-col items-center md:flex md:flex-row md:gap-1">
           <svg
             viewBox="0 0 512 512"
