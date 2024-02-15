805bb57d75bb87bb7a29957e365c91a22bca3d77
Angel Ugarte <fredanup@hotmail.com>
2024-02-15 17:14:17 -0500

    Mejoras en los enlaces para compartir

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -16,7 +16,7 @@ const MyApp: AppType<{ session: Session | null }> = ({
         <title>Somos Leandro</title>
         <meta
           name="description"
-          content="Aplicación de contratación de músicos"
+          content="Una aplicación para contratar músicos profesionales"
         />
         <link rel="shortcut icon" href="/favicon.ico" />
         <link rel="mask-icon" href="/icons/mask-icon.svg" color="#FFFFFF" />
@@ -25,30 +25,36 @@ const MyApp: AppType<{ session: Session | null }> = ({
         <link
           rel="apple-touch-icon"
           sizes="152x152"
-          href="/icons/touch-icon-ipad.png"
+          href="/icons/icon-152x152.png"
         />
         <link
           rel="apple-touch-icon"
           sizes="180x180"
-          href="/icons/touch-icon-iphone-retina.png"
+          href="/icons/logo-180x180.png"
         />
         <link
           rel="apple-touch-icon"
           sizes="167x167"
-          href="/icons/touch-icon-ipad-retina.png"
+          href="/icons/logo-167x167.png"
         />
         <link rel="manifest" href="/manifest.json" />
         <meta name="twitter:card" content="summary" />
-        <meta name="twitter:url" content="https://yourdomain.com" />
-        <meta name="twitter:title" content="My awesome PWA app" />
-        <meta name="twitter:description" content="Best PWA app in the world!" />
+        <meta name="twitter:url" content="https://www.somosleandro.com" />
+        <meta name="twitter:title" content="Somos Leandro" />
+        <meta
+          name="twitter:description"
+          content="Una aplicación para contratar músicos profesionales"
+        />
         <meta name="twitter:image" content="/icons/twitter.png" />
-        <meta name="twitter:creator" content="@DavidWShadow" />
+        <meta name="twitter:creator" content="Ángel Ugarte" />
         <meta property="og:type" content="website" />
-        <meta property="og:title" content="My awesome PWA app" />
-        <meta property="og:description" content="Best PWA app in the world!" />
-        <meta property="og:site_name" content="My awesome PWA app" />
-        <meta property="og:url" content="https://yourdomain.com" />
+        <meta property="og:title" content="Somos Leandro" />
+        <meta
+          property="og:description"
+          content="Una aplicación para contratar músicos profesionales"
+        />
+        <meta property="og:site_name" content="Somos Leandro" />
+        <meta property="og:url" content="https://www.somosleandro.com" />
         <meta property="og:image" content="/icons/og.png" />
         <meta
           name="facebook-domain-verification"
@@ -57,37 +63,37 @@ const MyApp: AppType<{ session: Session | null }> = ({
         {/* add the following only if you want to add a startup image for Apple devices. */}
         <link
           rel="apple-touch-startup-image"
-          href="/images/apple_splash_2048.png"
+          href="/icons/logo-2048x2732.png"
           sizes="2048x2732"
         />
         <link
           rel="apple-touch-startup-image"
-          href="/images/apple_splash_1668.png"
+          href="/icons/logo-1668x2224.png"
           sizes="1668x2224"
         />
         <link
           rel="apple-touch-startup-image"
-          href="/images/apple_splash_1536.png"
+          href="/icons/logo-1536x2048.png"
           sizes="1536x2048"
         />
         <link
           rel="apple-touch-startup-image"
-          href="/images/apple_splash_1125.png"
+          href="/icons/logo-1125x2436.png"
           sizes="1125x2436"
         />
         <link
           rel="apple-touch-startup-image"
-          href="/images/apple_splash_1242.png"
+          href="/icons/logo-1242x2208.png"
           sizes="1242x2208"
         />
         <link
           rel="apple-touch-startup-image"
-          href="/images/apple_splash_750.png"
+          href="/icons/logo-750x1334.png"
           sizes="750x1334"
         />
         <link
           rel="apple-touch-startup-image"
-          href="/images/apple_splash_640.png"
+          href="/icons/logo-640x1136.png"
           sizes="640x1136"
         />
       </Head>
